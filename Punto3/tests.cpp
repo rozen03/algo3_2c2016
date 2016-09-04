cd9e2f9593ced7e3115c6fc3839e64422fa69eb0
Jazz <jazminalvarezvico@gmail.com>
2016-09-04 18:49:16 -0300

    test p3

--- Punto3/tests.cpp
+++ Punto3/tests.cpp
@@ -102,18 +102,99 @@ void testObjMejorCaso(int rep){
 	res.close();
 }
 
-void testPesoMoch(int rep){//aca nos aseguramos que todos los objetos entren en la mochila mas chica, asi no varia dependiendo de los objetos que entrar
-	/*
-	 *quiero que dado cierto p me devuelva (p, tp1, tp2, tp3) donde tpi es tiempo promedio para i mochilas con p peso como producto.
-	 * me gustaria que no sea un producto trivial p*1*1 o p*1 a no ser que p sea 1, entonces si interesa... creo esto interesa?-> p/2*2*1
-	 */
-	 vector<int> d(3);
-	//ofstream res(
+void testPesoMoch(int rep){
+	ofstream res("resultadosPesoMoch.txt");
+
+	string t ="PesoMoch2-5-5.txt";
+	std:: ofstream ts(t);
+	ts<< "3 50 \n";
+	ts<<"2 5 5\n";	
+
+	string a="PesoMoch10-5.txt";
+	std::ofstream as(a);	
+	as<< "2 50 \n";
+	as<<"10 5\n";
+
+	string b="PesoMoch2-25.txt";
+	std::ofstream bs(b);
+	bs<< "2 50 \n";
+	bs<<"2 25\n";			
+	
+	string c="PesoMoch50.txt";
+	std::ofstream cs(c);
+	cs<< "1 50 \n";
+	cs<<"50 \n"; 
+
+	srand(time(NULL));
+	for(int j=0; j<50;j++){
+		int c=1;
+		int p= rand()%5+1;
+		int v= rand()%10000000+1;
+		ts<< c <<" "<< p<< " "<< v <<endl;
+		as<< c <<" "<< p<< " "<< v <<endl;
+		bs<< c <<" "<< p<< " "<< v <<endl;
+		cs<< c <<" "<< p<< " "<< v <<endl;	
+	}
+	ts.close();
+	as.close();
+	bs.close();
+	cs.close();
+	res<<"2-5-5 \n";
+	int sol= 0;
+	for(int i = 0; i <= rep; i++){
+		int solpar;
+		auto start = ya();
+		solucion(t);
+		auto end = ya();
+		solpar = chrono::duration_cast<std::chrono::nanoseconds>(end-start).count() / rep;
+		sol = sol +solpar;
+		}
+	res<<sol<<" & \n";
+	ts.close();
+	res<<"10-5\n";
+	sol= 0;
+	for(int i = 0; i <= rep; i++){
+		int solpar;
+		auto start = ya();
+		solucion(a);
+		auto end = ya();
+		solpar = chrono::duration_cast<std::chrono::nanoseconds>(end-start).count() / rep;
+		sol = sol +solpar;
+		}
+	res<<sol<<" & \n";
+	as.close();
+	res<<"2-25\n";
+	sol= 0;
+	for(int i = 0; i <= rep; i++){
+		int solpar;
+		auto start = ya();
+		solucion(b);
+		auto end = ya();
+		solpar = chrono::duration_cast<std::chrono::nanoseconds>(end-start).count() / rep;
+		sol = sol +solpar;
+		}
+	res<<sol<<" & \n";
+	bs.close();
+	res<<"50\n";
+	sol= 0;
+	for(int i = 0; i <= rep; i++){
+		int solpar;
+		auto start = ya();
+		solucion(c);
+		auto end = ya();
+		solpar = chrono::duration_cast<std::chrono::nanoseconds>(end-start).count() / rep;
+		sol = sol +solpar;
+		}
+	res<<sol<<" & \n";
+	cs.close();
+	res.close();
+	
 }
 
 int main(){
 	testObjAleatorios(1000);
-	testObjPesoRango(100);
+	testObjPesoRango(1000);
 	testObjMejorCaso(1000);
+	testPesoMoch(1000);
 return 0;
 }
