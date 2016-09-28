95f0da929b320a585c986477c303d812f14c0384
uriel <urielrozenberg@hotmail.com>
2016-09-27 21:46:30 -0300

    mas bugs

--- Tp2/Punto1/testear.sh
+++ Tp2/Punto1/testear.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 rm ej1
-g++ ej1.cpp -fstack-protector-all -g -std=c++11 -O3 -Ofast -o ej1
-cat test.txt | ./ej1 
+#g++ ej1.cpp -fstack-protector-all -g -std=c++11 -O3 -Ofast -o ej1
+g++ ej1.cpp -g  -o ej1
+cat test.txt | ./ej1
