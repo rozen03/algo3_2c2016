b7bad5ccbb98fd3da37fd7c185893f2d74323398
Jazmin Vico <jvico@ws3.labo2>
2016-10-18 18:58:39 -0300

    nos persiguen

--- Tp3/Punto1/pto1.cpp
+++ Tp3/Punto1/pto1.cpp
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <utility>
 #include <limits>
+#include <math>
 
 using namespace std;
 
@@ -48,28 +49,44 @@ int main(){
 }
 
 void BT(){
-
+	//Quiero cortar o en el caso de que ya hay una solucion mejor o cuando ya recorri todos los gimnasios.
 	if (MinActual>= MinGlobal || GimRecorridos == Gimnasios.size()){
 		return;
 	}
-
+ 	//Voy A un gimnasio o a una pokeparada.
 	for (int i = 0; i < Maximo(Gimnasios.size(), PokeParadas.size()) ; ++i){
-			if(!RecorridoGlobal.size()){
-				if(i<Gimnasios.size()){
-					nodo gim = Gimnasios[i];
-					if (puedoIrG(gim)){
-						gim.Recorrer()=true;
-						voyGym(gim);
-						gim.Recorrer()=false;
+			//esto elige de donde salimos
+			if(RecorridoActual.empty()) {
+				if(i<PokeParadas.size()){
+				nodo pp = PokeParadas[i];
+				//si es posible ir con el gimnasio, marco y voy y desmarco
+					if(puedoIrPP(pp)){
+						pp.Recorrer()=true;
+						RecorridoActual.push_back(pp.Indice());
+						voyPP(pp);
+						pp.Recorrer()=false;
+						RecorridoActual.pop_back();
 					}
+			 	}	
+			}	
+
+
+			if(i<Gimnasios.size()){
+				nodo gim = Gimnasios[i];
+				//si es posible ir con el gimnasio, marco y voy y desmarco
+				if (puedoIrG(gim)){
+					gim.Recorrer()=true;
+
+					voyGym(gim);
+					gim.Recorrer()=false;
 				}
 			}
+			//Voy a pokeparadas
 			if(i<PokeParadas.size()){
 				nodo pp = PokeParadas[i];
-				if(puedoIrPP()){
-					pp.Recorrer()=true;
+				//si es posible ir a pokeparada, marco y voy y desmarco
+				if(puedoIrPP(pp)){
 					voyPP(pp);
-					pp.Recorrer()=false;
 				}
 			}
 			
@@ -78,5 +95,48 @@ void BT(){
 	return;
 }
 
+bool puedoIrPP( nodo p){
+	return !mochila.EstaLLena() && (p.Recorrido() == false);
+}
+
+bool puedoIrG(nodo p){
+	return mochila.DamePeso() >= -(p.DamePociones()) && (p.Recorrido() == false);
+}
+
+int distancia(nodo o, nodo f) {
+	int x = o.CordenadaX() - f.CordenadaX();
+	int y = o.CordenadaY() - f.CordenadaY();
+	int pre = pow(x, 2) + pow(y, 2);
+	return sqrt(pre);
+}
 
 
+void voyPP(nodo p) {
+	pp.Recorrer()=true;
+	nodo origen = BuscarNodo(RecorridoActual.back());
+	int dist = Distancia(origen, p)
+	MinActual += dist;
+	RecorridoActual.push_back(p.Indice());
+	int pociones = p.DamePociones();
+	mochila.usarMochila(pociones);
+	BT();
+	MinActual -= dist;
+	RecorridoActual.pop_back();
+	pp.Recorrer()=false;
+	RecorridoActual.push_back(p.Indice());
+	mochila.usarMochila(-pociones);
+}
+
+void voyGym(nodo g) {
+	pp.Recorrer()=true;
+	nodo origen = BuscarNodo(RecorridoActual.back());
+	int dist = Distancia(origen, p)
+	MinActual += dist;
+	RecorridoActual.push_back(p.Indice());
+	BT();
+	MinActual -= dist;
+	RecorridoActual.pop_back();
+	pp.Recorrer()=false;
+	RecorridoActual.push_back(p.Indice());
+
+} 
