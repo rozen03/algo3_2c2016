1cd1e397ae1bea393b4eb2d1569d32822b22ea9c
Jazmin Vico <jvico@ws3.labo2>
2016-10-18 18:01:53 -0300

    pusheo para marce

--- Tp3/Punto1/pto1.cpp
+++ Tp3/Punto1/pto1.cpp
@@ -12,12 +12,71 @@ using namespace std;
 //Statics
 static int MAX = numeric_limits<int>::max();
 
+//Tipos
+typedef vector<int> vint;
+typedef vector<nodo > vnod;
+
+//Statics
+static int MAX = numeric_limits<int>::max();
+
 //variables globales
 int MinGlobal;
 int MinActual;
+vint RecorridoGlobal;
+vint RecorridoActual;
+vnod PokeParadas;
+vnod Gimnasios;
+int GimRecorridos;
+Mochila mochila;
 
+
+//declaracion de funciones
+void BT();
+void LecturaDeDatos();
+void voyPP();
+void voyGym(); 
+bool puedoIrG();
+bool puedoIrPP();
+int distancia();
+
+
+//funciones
 
 int main(){
-
+	MinGlobal = MAX;
 
 }
+
+void BT(){
+
+	if (MinActual>= MinGlobal || GimRecorridos == Gimnasios.size()){
+		return;
+	}
+
+	for (int i = 0; i < Maximo(Gimnasios.size(), PokeParadas.size()) ; ++i){
+			if(!RecorridoGlobal.size()){
+				if(i<Gimnasios.size()){
+					nodo gim = Gimnasios[i];
+					if (puedoIrG(gim)){
+						gim.Recorrer()=true;
+						voyGym(gim);
+						gim.Recorrer()=false;
+					}
+				}
+			}
+			if(i<PokeParadas.size()){
+				nodo pp = PokeParadas[i];
+				if(puedoIrPP()){
+					pp.Recorrer()=true;
+					voyPP(pp);
+					pp.Recorrer()=false;
+				}
+			}
+			
+				
+	}	
+	return;
+}
+
+
+
---
+++ Tp3/Punto1/pto1.cpp~
@@ -0,0 +1,23 @@
+#include "clases.h"
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <set>
+#include <tuple>
+#include <utility>
+#include <limits>
+
+using namespace std;
+
+//Statics
+static int MAX = numeric_limits<int>::max();
+
+//variables globales
+int MinGlobal;
+int MinActual;
+
+
+int main(){
+
+
+}
