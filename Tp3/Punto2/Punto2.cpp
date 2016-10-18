ebcd5d01e20c8520d6f20d877d531f53aa3bac0b
Marcelo Andres Pedraza <mpedraza@ws15.labo6>
2016-10-18 20:00:28 -0300

    Falta llenar unas cosas. Tengo un probleema grave , no se si usar punteros o no.

--- Tp3/Punto2/Punto2.cpp
+++ Tp3/Punto2/Punto2.cpp
@@ -22,23 +22,81 @@ vnod Gimnasios;
 int xactual;
 int yactual;
 //funciones
-nodo*	GimMasCercano(); //devuelve el  gimnasio mas cercano. Si devuelve 0 es porque no se le puede ganar
-nodo*	PokeParadaMasCercana(); //devuelve el id de la pokeparada mas cercana.
-void	moverse(*nodo);//actualiza la posicion actual, y el recorrido
+nodo	GimMasCercano(); //devuelve el  gimnasio mas cercano. Si devuelve 0 es porque no se le puede ganar
+nodo	PokeParadaMasCercana(); //devuelve el id de la pokeparada mas cercana.
+void	moverse(*nodo);//actualiza la posicion actual, y el recorrido, y elimina el nodo de vector correspondiente.
+float	distancia(*nodo); //calcula la distancia desde la posicion actual hasta el nodo
 
 int main(){
-	//comienza en ALGUN LADO. Por ej; la primera pokeparada
+	//comienza en ALGUN LADO. Por ej; la primera pokeparada tambieeeeen fijarse si "es posible"
 	/*
 		ARMAR ESTA COSA
 	*/
-	nodo* proxLugar;
+	nodo proxLugar;
 
 	while(!Gimnasios.empty()){//mientras sigan existiendo gimnasios que no pasaron  
 		proxLugar = GimMasCercano();
-		if(proxGim==null){ //no le puedo ganar al gimnasio mas cercano
+		if(proxGim==0){ //no le puedo ganar al gimnasio mas cercano
 			proxLugar = PokeParadaMasCercana();
 		}
-
+		moverse(proxLugar);
 
 	}
+
+
+	//Devolver distanciaRecorrida y RecorridoActual
+}
+
+
+nodo GimMasCercano(){//AAAAA NO SE SI USAR PUNTEROS
+	nodo min = 0;
+	for (int i = 0; i < Gimnasios.size(); ++i){
+		if(mochila.DameCapacidad()>=Gimnasios[i].damePociones()){//verifico que puedo vencer este gimnacio 
+			if(min==0){
+				min = Gimnasios[i];
+			}
+			else{
+				if(distancia(min)>distancia(Gimnasios[i])){
+					min = Gimnasios[i];		
+				}
+			}
+		}
+	}
+
+	return min;
+
+}
+
+nodo PokeParadaMasCercana(){
+	nodo min =0;
+
+	for (int i = 0; i < PokeParadas.size(); ++i){
+		if(min==0){
+				min = PokeParadas[i];
+			}
+			else{
+				if(distancia(min)>distancia(PokeParadas[i])){
+					min = PokeParadas[i];		
+				}
+		}
+	}
+	return min;
+}
+
+
+float distancia(nodo f) {
+	int x = xactual - f.CordenadaX();
+	int y = xactual - f.CordenadaY();
+	int pre = pow(x, 2) + pow(y, 2);
+	return sqrt(pre); //esto NO devuelve un int....
+}
+
+void moverse(nodo& lugar){
+	/*	To do: 
+		Actualizar x e y
+		eliminar del vector correspondiente el lugar al que voy
+		actualizar recorrido actual y distancia actual
+		vaciar/llenar la mochila
+	*/
+
 }
