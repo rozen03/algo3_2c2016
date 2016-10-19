8943ce0e0987ecb5f2daef1ba0348c2461b913bb
Marcelo <marcelopedraza314@gmail.com>
2016-10-18 22:54:32 -0300

    casi la termine. Tengo un problema grave de punteros y tipos

--- Tp3/Punto2/Punto2.cpp
+++ Tp3/Punto2/Punto2.cpp
@@ -1,4 +1,4 @@
-#include "clases.h"
+#include "../clases.h"
 #include <iostream>
 #include <chrono>
 #include <vector>
@@ -11,28 +11,28 @@ using namespace std;
 
 //Tipos
 typedef vector<int> vint;
-typedef vector<nodo > vnod;
+typedef vector<Nodo > vnod;
 
 //Variables
-Mochila mochila;
-vint RecorridoActual;
-int distanciaRecorrida
+Mochila moch(0);
+vint Recorrido;
+float distanciaRecorrida;
 vnod PokeParadas;
 vnod Gimnasios;
 int xactual;
 int yactual;
 //funciones
-nodo	GimMasCercano(); //devuelve el  gimnasio mas cercano. Si devuelve 0 es porque no se le puede ganar
-nodo	PokeParadaMasCercana(); //devuelve el id de la pokeparada mas cercana.
-void	moverse(*nodo);//actualiza la posicion actual, y el recorrido, y elimina el nodo de vector correspondiente.
-float	distancia(*nodo); //calcula la distancia desde la posicion actual hasta el nodo
+Nodo	GimMasCercano(); //devuelve el  gimnasio mas cercano. Si devuelve 0 es porque no se le puede ganar
+Nodo	PokeParadaMasCercana(); //devuelve el id de la pokeparada mas cercana.
+void	moverse(*Nodo);//actualiza la posicion actual, y el recorrido, y elimina el Nodo de vector correspondiente.
+float	dist(*Nodo); //calcula la distancia desde la posicion actual hasta el Nodo
 
 int main(){
 	//comienza en ALGUN LADO. Por ej; la primera pokeparada tambieeeeen fijarse si "es posible"
 	/*
 		ARMAR ESTA COSA
 	*/
-	nodo proxLugar;
+	Nodo* proxLugar=0;
 
 	while(!Gimnasios.empty()){//mientras sigan existiendo gimnasios que no pasaron  
 		proxLugar = GimMasCercano();
@@ -48,16 +48,16 @@ int main(){
 }
 
 
-nodo GimMasCercano(){//AAAAA NO SE SI USAR PUNTEROS
-	nodo min = 0;
+Nodo GimMasCercano(){//AAAAA NO SE SI USAR PUNTEROS
+	Nodo min = 0;
 	for (int i = 0; i < Gimnasios.size(); ++i){
-		if(mochila.DameCapacidad()>=Gimnasios[i].damePociones()){//verifico que puedo vencer este gimnacio 
+		if(moch.DamePeso()>=Gimnasios[i].damePociones()){//verifico que puedo vencer este gimnacio 
 			if(min==0){
-				min = Gimnasios[i];
+				min = $Gimnasios[i];
 			}
 			else{
-				if(distancia(min)>distancia(Gimnasios[i])){
-					min = Gimnasios[i];		
+				if(dist(min)>dist(Gimnasios[i])){
+					min = &Gimnasios[i];		
 				}
 			}
 		}
@@ -67,16 +67,16 @@ nodo GimMasCercano(){//AAAAA NO SE SI USAR PUNTEROS
 
 }
 
-nodo PokeParadaMasCercana(){
-	nodo min =0;
+Nodo* PokeParadaMasCercana(){
+	Nodo* min =0;
 
 	for (int i = 0; i < PokeParadas.size(); ++i){
 		if(min==0){
-				min = PokeParadas[i];
+				min = &PokeParadas[i];
 			}
 			else{
-				if(distancia(min)>distancia(PokeParadas[i])){
-					min = PokeParadas[i];		
+				if(dist(min)>dist(PokeParadas[i])){
+					min = &PokeParadas[i];		
 				}
 		}
 	}
@@ -84,19 +84,45 @@ nodo PokeParadaMasCercana(){
 }
 
 
-float distancia(nodo f) {
-	int x = xactual - f.CordenadaX();
-	int y = xactual - f.CordenadaY();
+float dist(Nodo* f) {
+	int x = xactual - f<-CordenadaX();
+	int y = xactual - f<-CordenadaY();
 	int pre = pow(x, 2) + pow(y, 2);
 	return sqrt(pre); //esto NO devuelve un int....
 }
 
-void moverse(nodo& lugar){
-	/*	To do: 
+void moverse(Nodo & lugar){
+	
+	/*	Lo que hace esta funcion: 
 		Actualizar x e y
 		eliminar del vector correspondiente el lugar al que voy
 		actualizar recorrido actual y distancia actual
 		vaciar/llenar la mochila
 	*/
+
+	nuevo = lugar; //copio el Nodo
+	
+	//elimino el Nodo de la lista correspondiente
+	if(lugar.EsGim()){
+		Gimnasios.erase(lugar);
+	}
+	else {
+		PokeParadas.erase(lugar);	
+	}
+
+	//lleno/vacio la mochila
+	moch.usarMochila(nuevo.damePociones());
+
+	//lo agrego al recorrido  y la distancia que estoy haciendo
+	Recorrido.push_back(nuevo);
+	distanciaRecorrida =+ distancia(nuevo);
+
+	//actualizo x e y
+	xactual = nuevo.CordenadaX();
+	yactual = nuevo.CordenadaY();
+
+
+	
+
 
 }
