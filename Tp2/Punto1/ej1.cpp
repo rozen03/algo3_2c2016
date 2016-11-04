bd85569134306ff92d70d40f47f0a6a18b6df0d6
Javiercct <javiercortescondetito@gmail.com>
2016-11-03 23:08:02 -0300

    la maldicion del 1

--- Tp2/Punto1/ej1.cpp
+++ Tp2/Punto1/ej1.cpp
@@ -20,6 +20,34 @@ void Imprimir(vector<Nodo *> &nodos) {
 	}
 }
 
+void MostrarNodo(Nodo * nodo){
+	cout<< nodo -> indice<<" ("<<nodo ->nivel<<") ";
+	if(nodo ->esPared)cout<<"P ";
+	if(nodo ->esFinal) cout<<"F ";
+}
+
+void MostrarVecino(Nodo * n){
+	vector<Eje *> ejes = n ->ejejes;
+	cout<<"[";
+	for(int i = 0; i<ejes.size(); i++){
+		Eje * ej = ejes[i];
+		Nodo * nodAux = ej ->dameElOtroNodoPorfa(n);
+		MostrarNodo(nodAux);
+		cout<<", ";
+	}
+	cout<<"]";
+}
+
+void MostrarCopia(vector<Nodo *>  nodos){
+	for(int i = 0; i< nodos.size(); i++){
+		Nodo * aux = nodos[i];
+		MostrarNodo(aux);
+		cout<<": ";
+		MostrarVecino(aux);
+		cout<<endl;
+	}
+}
+
 char DarLetra(Nodo * aux){
 	char res = '.';
 	if(aux -> esPared){
@@ -52,9 +80,6 @@ int Bfs(vector<Nodo *> &nodos) {
 			orden[nod] = next;
 			next++;
 			Nodo *nodin = nodos[nod];
-			cout<<"quiero ver cual es el nodo que estoy actualizando"<<endl;
-			cout<<DarLetra(nodin)<<" su indice "<<nodin -> indice << " su nivel "<<nodin -> nivel;
-			cout<<" es final? "<< nodin -> esFinal << " esPared? "<< nodin -> esPared<<endl;
 			vector<Eje *> ejesNodin = nodin->ejejes;
 			// me fijo si es final, si lo es corto el bfs por que ya encontre la
 			// primer solucion y ya marque todo lo que tenia que marcar.
@@ -66,9 +91,6 @@ int Bfs(vector<Nodo *> &nodos) {
 			for (unsigned int i = 0; i < ejesNodin.size(); i++) {
 				Eje *ejeaux = ejesNodin[i];
 				Nodo *nodoaux = ejeaux->dameElOtroNodoPorfa(nodin);
-				cout<<"quiero ver que nodos agrego o no "<<endl;
-			    cout<<DarLetra(nodoaux)<<" su indice "<<nodoaux -> indice << " su nivel "<<nodoaux -> nivel;
-			    cout<<" es final? "<< nodoaux -> esFinal << " esPared? "<< nodoaux -> esPared<<endl;
 				int acomp = nodoaux->indice;
 				// si ya lo tagee o estoy bajando a una instancia invalida, reconstruir
 				// una pared
@@ -78,11 +100,9 @@ int Bfs(vector<Nodo *> &nodos) {
 				// subir de nivel
 				if (nodoaux->nivel == nodin->nivel && nodoaux->esPared)
 				continue;
-				cout<<"agregue al nodo "<<acomp<<endl;
 				tuple<int, int> arist(acomp, nod);
 				list.push(arist);
 			}
-			cout<<"Voy al prox elemento de la lista"<<endl;
 		}
 	}
 
@@ -134,7 +154,7 @@ void mostrarNiveles(vector<Nodo *> nodos, int p, int f, int c){
 	}
 }
 
-int Solucion(vector<Nodo *> &nodos, vector<Eje *> &ejes, int p, int f, int c) {
+int Solucion(vector<Nodo *> &nodos, vector<Eje *> &ejes, int p) {
 	int res = p * 2 * nodos.size();
 	// quiero una res que sea lo mas grande posible, probablemente no tengo
 	// que multiplicar por p para tener algo lo suficientemente grande pero por
@@ -145,9 +165,12 @@ int Solucion(vector<Nodo *> &nodos, vector<Eje *> &ejes, int p, int f, int c) {
 	// había solucion.
 	int nores = res;
 	for (int i = 0; i < p; i++) {
-		//mostrarNiveles(nodos,p, f, c);
+		cout<<"el nivel es "<< i<<endl;
+		MostrarCopia(nodos);
 		clonarUltimoNivel(nodos, ejes);
 	}
+	cout<<"el nivel es "<<p<<endl;
+	MostrarCopia(nodos);
 	res = Bfs(nodos);
 	if (res == nores) {
 		res = -1;
@@ -158,12 +181,9 @@ int Solucion(vector<Nodo *> &nodos, vector<Eje *> &ejes, int p, int f, int c) {
 int main(int argc, char *argv[]) {
   std::vector<Nodo *> nodos;
   std::vector<Eje *> ejes;
-  int f;
-  int c;
   int p = 0;
-  p = parsearInput(nodos,ejes,1, f, c);
-  cout<< f <<" "<<c<<endl;
-  int res = Solucion(nodos, ejes, p, f, c);
+  p = parsearInput(nodos,ejes,1);
+  int res = Solucion(nodos, ejes, p);
   cout<<res<<endl;
 
   return 0;
